2954709eb0fdfd5cb907f6f160128b88db70cfdf
Tynash <89925268+TinasheGuni@users.noreply.github.com>
2024-10-07 10:20:07 +0200

    routine update

--- 2011_10_Assignment_02/03_Money/main.cpp
+++ 2011_10_Assignment_02/03_Money/main.cpp
@@ -11,7 +11,13 @@ int main()
     Money m3(5, 15);
 
     m1 = m2.Plus(m3);
-    cout <<  m1 <<
+    cout << m1 << " + " << m2 << " gives " << m1.Plus(m2) << endl;
+    m1 = m2 + m3;
+    cout << m2 << " + " << m3 << " gives " << m1 << endl;
+    if (m2.GreaterThan(m1))
+        cout << m2 << " is greater than " << m1 << endl;
+    else
+        cout << m2 << " is less than " << m1 << endl;
 
     return 0;
 }
