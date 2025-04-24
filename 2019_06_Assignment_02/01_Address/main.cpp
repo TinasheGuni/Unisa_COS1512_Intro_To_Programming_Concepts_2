25ca6f5529857b8fbc8e803b74c9502a8d4b3dfc
Tinashe Guni <tinashe.gunin@gmail.com>
2025-04-24 13:31:02 +0200

    update

---
+++ 2019_06_Assignment_02/01_Address/main.cpp
@@ -0,0 +1,18 @@
+// This program shows how to use a class
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Address {
+private:
+    string name1, name2;
+    string cityName;
+    int number, code;
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
