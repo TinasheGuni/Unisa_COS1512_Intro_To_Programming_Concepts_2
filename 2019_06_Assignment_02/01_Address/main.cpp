c59da8969c54a9903e9076e5712fa2aa876a7416
Tinashe Guni <tinashe.gunin@gmail.com>
2025-04-24 13:46:48 +0200

    update

--- 2019_06_Assignment_02/01_Address/main.cpp
+++ 2019_06_Assignment_02/01_Address/main.cpp
@@ -5,6 +5,11 @@
 using namespace std;
 
 class Address {
+public:
+    void input();
+    void output();
+    void set (string streetName1, string streetName2, int streetNumber, int postalCode, string city);
+
 private:
     string name1, name2;
     string cityName;
@@ -13,6 +18,39 @@ private:
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Address address;
+    cout << "\tAddress\n\n";
+    address.input();
+
+    cout << "\nThe Address is: \n";
+    address.output();
+
     return 0;
+}
+
+void Address::input() {
+    cout << "Enter Street Name: \n";
+    cin >>name1>>name2;
+    cout << "Enter Street Number: \n";
+    cin >> number;
+    cout << "Enter city name: \n";
+    cin >> cityName;
+    cout << "Enter Postal code: \n";
+    cin >> code;
+}
+
+void Address::output() {
+    cout<< "Street Name\t: " << name1 << name2 << endl
+        << "Street Number\t: " << number << endl
+        << "City\t\t: " << cityName << endl
+        << "Postal Code\t: " << code << endl;
+}
+
+void Address::set(string streetName, string streetName2, int streetNumber, int postalCode, string city) {
+    name1 = streetName;
+    name2 = streetName2;
+    cityName = city;
+    number = streetNumber;
+    code = postalCode;
+
 }
