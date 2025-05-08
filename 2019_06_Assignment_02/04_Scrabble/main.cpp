efbc40ec567cdfca832dfcc372fb90d9af0978e7
Tinashe Guni <tinashe.gunin@gmail.com>
2025-05-08 13:45:19 +0200

    update

--- 2019_06_Assignment_02/04_Scrabble/main.cpp
+++ 2019_06_Assignment_02/04_Scrabble/main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 class Scrabble {
 public:
     void GetLetters(string &let1, string &let2, string &let3);
-    void SetWord(string word, int rest);
 
 private:
     string letters1, letters2, letters3;
@@ -20,6 +19,8 @@ int main()
     string let1, let2, let3;
     int rest;
 
+    Scrabble Scrabble1;
+
     cout << "\nLets' play a bit of scrabble...You get 3 tries...." << endl;
     cout << "Make the longest word possible using the following letters: \n" << let1 << endl;
     cout << "You will be penalised for letters not used. \n";
@@ -27,5 +28,13 @@ int main()
 
     rest = 10 - word.size();
 
+    cout << Scrabble1 << endl;
+
     return 0;
+}
+
+void Scrabble::GetLetters(string &let1, string &let2, string &let3) {
+    letters1 = let1;
+    letters2 = let2;
+    letters3 = let3;
 }
